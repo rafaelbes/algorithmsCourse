3145ae4b9e41fd9fe7aa2aeec6f00436a66f4772
rafaelbes <rafaelufrn@gmail.com>
2021-02-25 16:14:01 -0300

    Adicionados lista7.sh e q2inicial.c

---
+++ repeticoesAninhadas/q2inicial.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char meses[][30] = {"Janeiro", "Fevereiro", "Marco", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+
+int main() {
+
+
+	return 0;
+}
